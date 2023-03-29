a4cc27621e268b738848a2b6daa021c469235b94
Rittwick Bhabak <mcs222054@cse.iitd.ac.in>
2023-03-29 17:20:15 +0530

    text and input boxes tut done and files renamed to lesson names

---
+++ 02_input_boxes_and_buttons.py
@@ -0,0 +1,62 @@
+import kivy 
+from kivy.app import App 
+from kivy.uix.label import Label 
+from kivy.uix.gridlayout import GridLayout # FloatLayout, BoxLayout, AnchorLayout, StackLayout, RelativeLayout
+from kivy.uix.textinput import TextInput 
+from kivy.uix.button import Button 
+
+
+class MyGridLayout(GridLayout):
+    # Initialize infinite keywords 
+    def __init__(self, **kwargs):
+        # Call grid layout constructor 
+        super(MyGridLayout, self).__init__(**kwargs) 
+
+        # Set columns 
+        self.cols = 2 
+
+        # Add widgets  # Text, TextInput, Button, Label
+        self.add_widget(Label(text='Name: '))
+        # Add Input Box 
+        self.name = TextInput(multiline=False) 
+        self.add_widget(self.name) 
+
+        # Add another widget
+        self.add_widget(Label(text='Favorite Pizza: '))
+        # Add Input Box 
+        self.pizza = TextInput(multiline=False) 
+        self.add_widget(self.pizza) 
+        
+        # Add another widget
+        self.add_widget(Label(text='Favorite Color: '))
+        # Add Input Box 
+        self.color = TextInput(multiline=False) 
+        self.add_widget(self.color) 
+
+        # Create a submit button 
+        self.submit = Button(text='Submit', font_size=32)
+        # Bind event to the button
+        self.submit.bind(on_press=self.press) 
+        self.add_widget(self.submit) 
+
+    def press(self, instance):
+        name = self.name.text 
+        pizza = self.pizza.text 
+        color = self.color.text 
+
+        # print(f"Hello {name}, you like {pizza} pizza and your favorite color is {color}!")
+        # print this to the screen 
+        self.add_widget(Label(text=f"Hello {name}, you like {pizza} pizza and your favorite color is {color}!"))
+        
+        # Clear the input boxes 
+        self.name.text = ''
+        self.pizza.text = ''
+        self.color.text = ''
+
+class MyApp(App):
+    def build(self):
+        # return Label(text="Hello Rittwick", font_size=72)
+        return MyGridLayout()
+    
+if __name__=='__main__':
+    MyApp().run()
