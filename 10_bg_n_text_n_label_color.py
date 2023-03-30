87af77dd375365fec7441e4d9ed439ddc66de3e8
rittwickbhabak <rittwick2019@gmail.com>
2023-03-30 11:54:30 +0530

    coloring labels and texts

---
+++ 10_bg_n_text_n_label_color.py
@@ -0,0 +1,18 @@
+from kivy.app import App 
+from kivy.uix.widget import Widget 
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder 
+
+Builder.load_file('10_design.kv')
+
+class MyLayout(Widget):
+
+    pass 
+
+class AwesomeApp(App):
+    def build(self):
+        # return Label(text="Hello Rittwick", font_size=72)
+        return MyLayout()
+    
+if __name__=='__main__':
+    AwesomeApp().run()
