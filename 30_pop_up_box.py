25ca22f57cd3993ffa657825e6a6fb01d7e0526f
Rittwick Bhabak <mcs222054@cse.iitd.ac.in>
2023-03-30 22:27:15 +0530

    pop box tut done

---
+++ 30_pop_up_box.py
@@ -0,0 +1,18 @@
+from kivy.app import App 
+from kivy.uix.widget import Widget 
+from kivy.lang import Builder 
+
+Builder.load_file("30_pop_up_box.kv") 
+
+class MyLayout(Widget):
+    pass 
+
+
+
+class AwesomeApp(App):
+    def build(self):
+        return MyLayout() 
+    
+
+if __name__=='__main__':
+    AwesomeApp().run() 
